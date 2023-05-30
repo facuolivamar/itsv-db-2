65f8803aa7b22a2ecb35b219c1768e9f0d242716
facuolivamar <facundoolivam@gmail.com>
2023-05-30 15:06:17 -0300

    repository code

--- Classes/Class03.sql
+++ Classes/Class03.sql
@@ -1,40 +1,85 @@
 use sakila;
 
-SHOW TABLES;
 
--- SELECT title, rating, length
--- FROM film
--- WHERE length > 100 AND rating = 'G';
+## Conditions
+```sql
+SELECT title, rating, length
+FROM film
+WHERE length > 100;
+```
+```sql
+SELECT title, `length` FROM film
+WHERE `length` BETWEEN 100 AND 120;
+```
 
--- SELECT title, `length` FROM film
--- WHERE `length` BETWEEN 100 AND 120;
+## Mutilple Tables
+```sql
+SELECT city, district
+FROM address, city
+WHERE address.city_id = city.city_id;
+```
 
--- SELECT city, district
--- FROM address, city
--- WHERE address.city_id = city.city_id;
+## Adding distinct
+```sql
+SELECT [DISTINCT] country, city
+FROM address, city, country
+WHERE address.city_id = city.city_id
+AND city.country_id = country.country_id;
+```
 
--- SELECT title, name, length
--- FROM film, `language`
--- WHERE film.language_id = language.language_id
--- AND film.`length` > 100 AND language.name = 'English'
+## Conditions with columns in different tables 
+```sql
+SELECT title, name
+FROM film, `language`
+WHERE film.language_id = language.language_id
+AND film.`length` > 100 AND language.name = 'English'
+```
 
--- SELECT title, film_category.category_id
---  FROM film, film_category, category
--- WHERE film.film_id = film_category.film_id 
---   AND film_category.category_id = category.category_id;
+## Ambigous column names
+```sql
+SELECT title, category_id
+ FROM film, film_category, category
+WHERE film.film_id = film_category.film_id 
+  AND film_category.category_id = category.category_id
+``` 
 
--- SELECT title, special_features, rental_rate, name
---  FROM film, film_category, category
--- WHERE film.film_id = film_category.film_id 
---   AND film_category.category_id = category.category_id
--- ORDER BY rental_rate ASC;
+## Adding Order BY
+```sql
+SELECT title, special_features, rental_rate, name
+ FROM film, film_category, category
+WHERE film.film_id = film_category.film_id 
+  AND film_category.category_id = category.category_id
+ORDER BY rental_rate DESC
+```
+### More than one column
+```sql
+SELECT title, special_features, rental_rate, name
+ FROM film, film_category, category
+WHERE film.film_id = film_category.film_id 
+  AND film_category.category_id = category.category_id
+ORDER BY rental_rate DESC, special_features ASC
+```
+## Using Limit
+```sql
+SELECT * FROM actor
+LIMIT 10;
+```
 
--- SELECT title, special_features, rental_rate, name
---  FROM film, film_category, category
--- WHERE film.film_id = film_category.film_id 
---   AND film_category.category_id = category.category_id
--- ORDER BY rental_rate DESC, special_features ASC;
+## Like
 
--- SELECT * FROM actor
--- LIMIT 10;
+```sql
+SELECT *
+FROM film
+WHERE special_features LIKE '%Trailers%'
+```
+When searching for characters _ %, they have to be escaped with \ (default escape character)
 
+```sql
+SELECT * FROM address
+WHERE address LIKE '%\_%';
+```
+## Arithmetics
+```sql
+SELECT title, description, rental_rate * 150 AS "In Pesos" 
+FROM film
+```
